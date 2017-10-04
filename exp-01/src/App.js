bd2d8ca1b2eb14ba87f43c323c32a26ccccb8aa0
Student <student@m-b2406.arts.local>
2017-10-04 16:22:59 +0100

    Added react router

--- exp-01/src/App.js
+++ exp-01/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Router, Route, Link } from 'react-router-dom'
+
+import VoiceMod from './VoiceMod';
+import Echo from './Echo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        // <ul>
+        //   <li><Link to="/">Home</Link></li>
+        //   <li><Link to="/voice">Voice</Link></li>
+        //   <li><Link to="/echo">echo</Link></li>
+        // </ul>
+        <Route path="/voice" component={VoiceMod} />
+        <Route path="/echo" component={Echo} />
       </div>
     );
   }
